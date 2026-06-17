0e9d831972fd74ce34f1de620f06151ab8c09931
onsChtioui <onschtioui1@gmail.com>
2026-06-17 15:19:54 +0100

    feat(auth): décorateur RequirePermission pour les permissions

---
+++ src/common/decorators/require-permission.decorator.ts
@@ -0,0 +1,2 @@
+// Alias pour compatibilité avec les modules clients/orders qui utilisent RequirePermission (sans s)
+export { RequirePermissions as RequirePermission } from './require-permissions.decorator';
